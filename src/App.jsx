92f3bc6e1aa3660db8cde81f5183c847438e57c1
adarsh <adarshchaturvedi0207@gmail.com>
2025-06-14 00:33:50 +0530

    unfortunetly delted the json file took long and again run it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -13,21 +13,17 @@ const AppLayout = () => {
     </div>
   );
 };
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  // {
-  //   path: "/contact",
-  //   element: <About />,
-  // },
-]);
+// const appRouter = createBrowserRouter([
+//   {
+//     path: "/",
+//     element:<AppLayout/>
+//   },
+//   {
+//     path: "/about",
+//     element:<About />
+//   }
+// ])
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //creates a dom in the browser
 
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<AppLayout />);
